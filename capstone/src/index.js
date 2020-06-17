b4dd446abd538329f536d4c726949ff4e26d4514
Cindy <cindyzhang977@gmail.com>
2020-06-17 21:04:00 +0000

    restructure files/folders

--- capstone/src/index.js
+++ capstone/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './webapp/index.css';
+import App from './webapp/App';
+import * as serviceWorker from './webapp/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
