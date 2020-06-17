3740dfa8052f99b091f308ef7ea140ed5603808d
Cindy <cindyzhang977@gmail.com>
2020-06-17 22:30:16 +0000

    initial set up react and servlet

--- capstone-app/target/capstone-1/my-app/src/App.js
+++ capstone-app/target/capstone-1/my-app/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  function handleClick() {
+    console.log("clicked")
+    fetch('http://localhost:8080/helloworld')
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json)
+        setText(json)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onclick={handleClick}>Fetch</button>
+      <div>{text}</div>
     </div>
   );
 }
